b6f745021394506a25a933bf2c6ec121fdf46886
cc.cheuk@gmail.com <cc.cheuk@gmail.com>
2024-02-20 13:22:36 +0000

    added item not leased between a period

--- backend/app/models/item.rb
+++ backend/app/models/item.rb
@@ -4,5 +4,10 @@ class Item < ApplicationRecord
   scope :leased_on, ->(date){joins(:lease_records).merge(LeaseRecord.has_lease_record_on(date))}
   scope :not_leased_on, ->(date){where.not(id: leased_on(date))}
   scope :available_and_not_leased_on, ->(date){where.not(id: leased_on(date)).available_on(date)}
+  scope :available_but_leased_on, ->(date){where(id: leased_on(date)).available_on(date)}
+  scope :leased_between, ->(start_date, end_date){joins(:lease_records).merge(LeaseRecord.has_lease_record_between(start_date, end_date))}
+  scope :not_leased_between, ->(start_date, end_date){where.not(id: leased_between(start_date, end_date))}
+  
+
 
 end
--- backend/app/models/lease_record.rb
+++ backend/app/models/lease_record.rb
@@ -2,5 +2,22 @@ class LeaseRecord < ApplicationRecord
   belongs_to :item
   scope :has_lease_record_on, ->(date){where(["date_from <= ? ", date]).where(["date_to >= ? ", date])}
   # scope :not_leased_on, ->(date){where.not(id: leased_on(date))}
-
+  scope :has_lease_record_end_between_the_requested_period, ->(start_date, end_date){
+    where(["date_to >= ? AND date_to <= ?", start_date, end_date])
+  }
+  scope :has_lease_record_covering_the_requested_period, ->(start_date, end_date){
+    where(["date_from <= ? AND date_to >= ? ", start_date, end_date])
+  }
+  scope :has_lease_record_within_the_requested_period, ->(start_date, end_date){
+    where(["date_from >= ? AND date_to <= ? ", start_date, end_date])
+  }  
+  scope :has_lease_record_start_between_the_requested_period, ->(start_date, end_date){
+    where(["date_from >= ? AND date_from <= ? ", start_date, end_date])
+  }    
+  scope :has_lease_record_between, ->(start_date, end_date){
+    has_lease_record_end_between_the_requested_period(start_date,end_date)
+    .or(has_lease_record_covering_the_requested_period(start_date, end_date))
+    .or(has_lease_record_within_the_requested_period(start_date, end_date))    
+    .or(has_lease_record_start_between_the_requested_period(start_date, end_date))
+  }
 end
