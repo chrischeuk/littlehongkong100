03f0e33138abfe2ac428ef7b7863a83b6e4ced3d
chrischeuk <cc.cheuk@gmail.com>
2024-05-16 06:53:34 +0000

    added markers in map and added 3d to map

--- frontend/src/views/Index.tsx
+++ frontend/src/views/Index.tsx
@@ -1,8 +1,22 @@
 import mapboxgl from 'mapbox-gl'
 import React, { useRef, useEffect, useState } from 'react'
+import { createRoot } from 'react-dom/client';
 
 mapboxgl.accessToken = 'pk.eyJ1Ijoid29yZHNvZm1vdXRoIiwiYSI6ImNrMmZ2MTNiZDBqczczY3A1azVxdnZsdDEifQ.l3j6LDp3rtjSRIKUonA7zg';
+var currentMarkers = [];
 
+const players = [
+    {
+        name: "JJ",
+        coordinates: [151.17, -33.5975],
+        description: "hello JJ"
+    },
+    {
+        name: "DD",
+        coordinates: [151.2093, -33.6],
+        description: "hello DD"
+    }
+]
 
 export default function index() {
     const mapContainer = useRef(null);
@@ -11,19 +25,43 @@ export default function index() {
     const [lat, setLat] = useState(-33.7);
     const [zoom, setZoom] = useState(11);
 
+    const Marker = ({ onClick, children, player }) => {
+        const _onClick = () => {
+            onClick(player.description);
+        };
+
+        return (
+            <div onClick={_onClick} className="marker  relative flex h-3 w-3">
+                <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-red-400 opacity-75"></span>
+                <span className="relative inline-flex rounded-full h-3 w-3 bg-red-500"></span>
+                {children}
+            </div>
+        );
+    };
+    const markerClicked = (title) => {
+        window.alert(title);
+    };
 
     useEffect(() => {
         if (map.current) return; // initialize map only once
         map.current = new mapboxgl.Map({
             container: mapContainer.current,
-            style: 'mapbox://styles/mapbox/streets-v12',
+            style: 'mapbox://styles/mapbox/satellite-streets-v12',
             center: [lng, lat],
             zoom: zoom,
+            pitch:60
 
         });
-        setTimeout(100)
+
 
         map.current.on("load", () => {
+            map.current.addSource('mapbox-dem', {
+                'type': 'raster-dem',
+                'url': 'mapbox://mapbox.terrain-rgb',
+                'tileSize': 512,
+                'maxzoom': 14
+            });
+            map.current.setTerrain({ 'source': 'mapbox-dem', 'exaggeration': 2 });
             map.current.addSource("100km", {
                 type: "vector",
                 url: "mapbox://wordsofmouth.46lv3lxu",
@@ -36,6 +74,7 @@ export default function index() {
                 paint: {
                     "line-color": "#0047AB",
                     "line-width": 4,
+                    'line-dasharray': [1, 1]
                 },
             })
             map.current.loadImage(
@@ -50,18 +89,69 @@ export default function index() {
                         "source-layer": "waypoints",
                         layout: {
                             'icon-image': 'custom-marker',
+                            'icon-size': 0.5,
                             'text-field': ['get', 'name'],
                             "text-size": 16,
-                            "text-offset": [0, -1.5],
+                            "text-offset": [0, -2],
                         },
                         paint: {
                             "text-color": "#1d1485",
                             "text-halo-color": "#ffffff",
-                            "text-halo-width": 0.5,
+                            "text-halo-width": 1,
                         },
                     })
                 })
+            map.current.addControl(new mapboxgl.NavigationControl(), "top-right");
+
+            // const marker1 = new mapboxgl.Marker()
+            //     .setLngLat([151.2093, -33.7])
+            //     .addTo(map.current)
+
+
+            // // Render custom marker components
+            // players.forEach((player) => {
+            //     // Create a React ref
+            //     const ref = React.createRef();
+            //     // Create a new DOM node and save it to the React ref
+            //     ref.current = document.createElement('div');
+            //     // Render a Marker Component on our new DOM node
+            //     createRoot(ref.current).render(
+            //         <Marker onClick={markerClicked} player={player} />
+            //     );
+
+            //     // Create a Mapbox Marker at our new DOM node
+            //     var oneMarker = new mapboxgl.Marker(ref.current)
+            //         .setLngLat(player.coordinates)
+            //         .addTo(map.current);
+            //     currentMarkers.push(oneMarker);
+            // });
         })
+
+        // return () => { map.current.remove() };
+    });
+    useEffect(() => {
+        map.current.on("load", () => {
+
+            // Render custom marker components
+            players.forEach((player) => {
+                // Create a React ref
+                const ref = React.createRef();
+                // Create a new DOM node and save it to the React ref
+                ref.current = document.createElement('div');
+                // Render a Marker Component on our new DOM node
+                createRoot(ref.current).render(
+                    <Marker onClick={markerClicked} player={player} />
+                );
+
+                // Create a Mapbox Marker at our new DOM node
+                var oneMarker = new mapboxgl.Marker(ref.current)
+                    .setLngLat(player.coordinates)
+                    .addTo(map.current);
+                currentMarkers.push(oneMarker);
+            });
+        })
+
+        // return () => { map.current.remove() };
     });
 
 
